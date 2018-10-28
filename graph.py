d9b9776e1a4673fb134bcb5f068eecb0b29b61e5
Mirko Mantovani <mirko.mantovani@mail.polimi.it>
2018-10-28 17:57:43 -0500

    Added dataset and stopwords, built graph data structure

---
+++ graph.py
@@ -0,0 +1,21 @@
+class UndirectedGraph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_node(self, node):
+        if node not in self.graph:
+            self.graph[node] = {}
+
+    def add_edge(self, i, j, weight):
+        if i not in self.graph:
+            self.add_node(i)
+        if j not in self.graph:
+            self.add_node(j)
+        self.graph[i][j] = weight
+        self.graph[j][i] = weight
+
+    def get_edge(self, i, j):
+        if i in self.graph:
+            if j in self.graph[i]:
+                return self.graph[i][j]
+        return 0
