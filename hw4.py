65ff5699f85ee575201eeca8453bca21405e5f8b
Mirko Mantovani <mirko.mantovani@mail.polimi.it>
2018-10-30 01:57:54 -0500

    Pagerank should work, problems with datasets of different dimensions, probably the professor gave us wrong data

--- hw4.py
+++ hw4.py
@@ -1,6 +1,7 @@
 # Mirko Mantovani
 
 import preprocess
+import page_rank
 import os
 
 STOP_WORDS_PATH = "./stopwords.txt"
@@ -12,7 +13,24 @@ vocabulary = {}
 # Point 1: word graph creation from documents
 tokenizer = preprocess.CustomTokenizer(STOP_WORDS_PATH)
 
-for filename in sorted(os.listdir(DOCS_PATH)):
-    if not filename.startswith('.'):
+# for filename in sorted(list(map(int, os.listdir(DOCS_PATH)))):
+#     print(filename)
+
+# docs = os.listdir(DOCS_PATH)
+# docs = [i for i in os.listdir(DOCS_PATH) if not i.startswith(".")]
+# for filename in sorted(list(map(int, [i for i in os.listdir(DOCS_PATH) if not i.startswith(".")]))):
+#     print(filename)
+
+for filename in list(map(str,sorted(list(map(int, [i for i in os.listdir(DOCS_PATH) if not i.startswith(".")]))))):
         documents.append(tokenizer.tokenize(open(DOCS_PATH + filename).read(), vocabulary))
 
+pageranker = page_rank.PageRank()
+p_rank = pageranker.page_rank(documents[0].graph, 100)
+
+import operator
+sorted_p = sorted(p_rank.items(), key=operator.itemgetter(1))
+
+for i in sorted_p:
+    print(i)
+# for document in documents:
+#     pageranker.page_rank(document.graph, 100)
--- page_rank.py
+++ page_rank.py
@@ -3,33 +3,32 @@ sys.setrecursionlimit(1500)
 
 
 class PageRank:
-    def __init__(self, word_graph, alpha=0.85):
+    def __init__(self, alpha=0.85):
         self.alpha = alpha
-        self.word_graph = word_graph
 
-    def s(self, i, p, previous_s):
+    def s(self, word_graph, i, p, previous_s):
 
-        return self.alpha * sum(self.word_graph.get_edge(j, i) / sum(self.word_graph.get_edge(j, k)
-                                                                     for k in self.word_graph.graph[j]) * previous_s[j]
-                                for j in self.word_graph.graph[i]) + (1 - self.alpha) * p[i]
+        return self.alpha * sum(word_graph.get_edge(j, i) / sum(word_graph.get_edge(j, k)
+                                                                     for k in word_graph.graph[j]) * previous_s[j]
+                                for j in word_graph.graph[i]) + (1 - self.alpha) * p[i]
 
-    def page_rank(self,  convergence):
+    def page_rank(self, word_graph, max_iterations):
 
         p = {}
         page_rank = {}
         last_page_rank = {}
 
-        for node in self.word_graph.graph:
-            p[node] = 1/len(self.word_graph.graph)
-            page_rank[node] = 1/len(self.word_graph.graph)
-            last_page_rank[node] = 1/len(self.word_graph.graph)
-        for iteration in range(0, convergence):
-            for i in self.word_graph.graph:
-                page_rank[i] = self.s(i, p, last_page_rank)
+        for node in word_graph.graph:
+            p[node] = 1/len(word_graph.graph)
+            page_rank[node] = 1/len(word_graph.graph)
+            last_page_rank[node] = 1/len(word_graph.graph)
+        for iteration in range(0, max_iterations):
+            for i in word_graph.graph:
+                page_rank[i] = self.s(word_graph,i, p, last_page_rank)
             #normalization and updating steps
-            total_weight = sum(page_rank[i]**2 for i in page_rank)**(1/2)
+            # total_weight = sum(page_rank[i]**2 for i in page_rank)**(1/2)
             for i in page_rank:
                 # ??
-                page_rank[i] = page_rank[i] / total_weight
+                # page_rank[i] = page_rank[i] / total_weight
                 last_page_rank[i] = page_rank[i]
         return page_rank
